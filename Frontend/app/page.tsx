c51f4beef7571483da46c2752e3301c95f034d2d
Nathan <nathan.torresc@gmail.com>
2025-04-17 16:28:41 -0700

    feat: actualizar componente Home para incluir encabezado y enlace de inicio de sesión

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello World</h1>
-    </main>
+    <header className="bg-gray-900 text-white flex justify-between items-center p-4">
+      <h1 className="text-3xl font-bold">Psicologia Educativa e infantil</h1>
+      <a href="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <p className="text-base font-semibold">Iniciar sesion</p>
+      </a>
+    </header>
   )
  
 }
