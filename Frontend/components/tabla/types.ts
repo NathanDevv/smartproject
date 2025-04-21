f1460c56cdf329fabd59ce98ea59d3cf19edee7f
AdrianEmaST <desarrolloadrianjr@gmail.com>
2025-04-21 12:40:14 -0300

    ADD: NewAPI

--- Frontend/components/tabla/types.ts
+++ Frontend/components/tabla/types.ts
@@ -1,15 +1,23 @@
-// tabla/types.ts
-
 export type User = {
-    id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-    address: {
-      city: string;
-    };
-    company: {
-      name: string;
+  id: number;
+  name: string; // "firstname lastname" todo en uno
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
     };
   };
-  
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
